998747d126d8d6f48cc97cbb4d2573e53e2c061f
Ameen shariff <ameenshariff0319@gmail.com>
2026-02-16 06:56:50 +0530

    d

--- server/app.js
+++ server/app.js
@@ -3,10 +3,93 @@ const express = require('express')
 const app = express();
 config('dotenv')
 const cors = require('cors')
+const mongoose = require("mongoose")
+const Users = require("./Model/notes")
 
 
 app.use(express.json());
 app.use(cors())
+
+
+
+
+
+let dbConnect = async()=>{
+    mongoose.connect(process.env.URL)
+    .then(console.log("db connected"))
+    .catch((err)=>{
+        console.log(err)
+    })
+
+}
+dbConnect()
+
+
+
+
+
+
+app.get("/notes",async(req,res)=>{
+    const notes = await Users.find({})
+    res.status(200).json(notes)
+})
+
+app.get("/notes/:id",async(req,res)=>{
+    const notes = await Users.findById(req.params.id)
+    res.status(200).json(notes)
+})
+app.post("/notes",async(req,res)=>{
+
+    const notes = await Users.create(req.body)
+    notes.save()
+
+    res.status(201).json({
+        msg:"successs ji"
+    })
+})
+
+app.put("/notes/:id",async(req,res)=>{
+    const notes = await Users.findByIdAndUpdate(req.params.id, req.body)
+
+    if(!notes){
+        res.json({msg:"not found"})
+    }
+
+    res.status(200).json({
+        msg:"done"
+    })
+})
+app.patch("/notes/:id",async(req,res)=>{
+    console.log(req.params.id)
+    const notes = await Users.findByIdAndUpdate(req.params.id,{title:req.body.title})
+    if(!notes){
+        res.json({
+            msg:"not foufd"
+        })
+    }
+
+    res.status(200).json({
+        msg:"done"
+    })
+})
+
+app.delete('/notes/:id',async(req,res)=>{
+
+    const notes = await Users.findById()
+    const deleteData = await Users.deleteOne({_id:req.params.id})
+
+    if(!deleteData){
+
+         res.json({
+            msg:"not foufd"
+        })
+
+    }
+
+    res.status(203).json({
+        msg:"deleted"
+    })
+})
 
 
 
